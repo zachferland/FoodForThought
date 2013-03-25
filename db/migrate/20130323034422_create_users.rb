9f916c9ef97cd8c7ddcd62f3518262d456263644
Zach Ferland <zachferland@gmail.com>
2013-03-24 20:10:27 -0400

    Start Mobile Version

---
+++ db/migrate/20130323034422_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :twitter
+      t.string :linkedin
+
+      t.timestamps
+    end
+  end
+end
